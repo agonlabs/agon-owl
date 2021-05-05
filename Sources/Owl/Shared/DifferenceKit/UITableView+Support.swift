bf0f269c0e7c6f5f92e5b27798e281d25d57e980
Alexander Shchavrovskiy <sapalt@yandex-team.ru>
2021-05-05 16:50:26 +0300

    fix batch updates

--- Sources/Owl/Shared/DifferenceKit/UITableView+Support.swift
+++ Sources/Owl/Shared/DifferenceKit/UITableView+Support.swift
@@ -79,56 +79,56 @@ internal extension UITableView {
 			setData(data)
 			return reloadData()
 		}
-				
-		for changeset in stagedChangeset {
-			if let interrupt = interrupt, interrupt(changeset), let data = stagedChangeset.last?.data {
-				setData(data)
-				return reloadData()
-			}
-			
-			_performBatchUpdates {
-				setData(changeset.data)
-				
-				if !changeset.sectionDeleted.isEmpty {
-					deleteSections(IndexSet(changeset.sectionDeleted), with: deleteSectionsAnimation())
-				}
-				
-				if !changeset.sectionInserted.isEmpty {
-					insertSections(IndexSet(changeset.sectionInserted), with: insertSectionsAnimation())
-				}
-				
-				if !changeset.sectionUpdated.isEmpty {
-					reloadSections(IndexSet(changeset.sectionUpdated), with: reloadSectionsAnimation())
-				}
-				
-				for (source, target) in changeset.sectionMoved {
-					moveSection(source, toSection: target)
-				}
-				
-				if !changeset.elementDeleted.isEmpty {
-					deleteRows(at: changeset.elementDeleted.map {
-						IndexPath(row: $0.element, section: $0.section)
-					}, with: deleteRowsAnimation())
-				}
-				
-				if !changeset.elementInserted.isEmpty {
-					insertRows(at: changeset.elementInserted.map {
-						IndexPath(row: $0.element, section: $0.section)
-					}, with: insertRowsAnimation())
-				}
-				
-				if !changeset.elementUpdated.isEmpty {
-					reloadRows(at: changeset.elementUpdated.map {
-						IndexPath(row: $0.element, section: $0.section)
-					}, with: reloadRowsAnimation())
-				}
-				
-				for (source, target) in changeset.elementMoved {
-					moveRow(at: IndexPath(row: source.element, section: source.section), to: IndexPath(row: target.element, section: target.section))
-				}
-			}
-		}
-	}
+        
+        _performBatchUpdates {
+            for changeset in stagedChangeset {
+                if let interrupt = interrupt, interrupt(changeset), let data = stagedChangeset.last?.data {
+                    setData(data)
+                    return reloadData()
+                }
+                
+                setData(changeset.data)
+                
+                if !changeset.sectionDeleted.isEmpty {
+                    deleteSections(IndexSet(changeset.sectionDeleted), with: deleteSectionsAnimation())
+                }
+                
+                if !changeset.sectionInserted.isEmpty {
+                    insertSections(IndexSet(changeset.sectionInserted), with: insertSectionsAnimation())
+                }
+                
+                if !changeset.sectionUpdated.isEmpty {
+                    reloadSections(IndexSet(changeset.sectionUpdated), with: reloadSectionsAnimation())
+                }
+                
+                for (source, target) in changeset.sectionMoved {
+                    moveSection(source, toSection: target)
+                }
+                
+                if !changeset.elementDeleted.isEmpty {
+                    deleteRows(at: changeset.elementDeleted.map {
+                        IndexPath(row: $0.element, section: $0.section)
+                    }, with: deleteRowsAnimation())
+                }
+                
+                if !changeset.elementInserted.isEmpty {
+                    insertRows(at: changeset.elementInserted.map {
+                        IndexPath(row: $0.element, section: $0.section)
+                    }, with: insertRowsAnimation())
+                }
+                
+                if !changeset.elementUpdated.isEmpty {
+                    reloadRows(at: changeset.elementUpdated.map {
+                        IndexPath(row: $0.element, section: $0.section)
+                    }, with: reloadRowsAnimation())
+                }
+                
+                for (source, target) in changeset.elementMoved {
+                    moveRow(at: IndexPath(row: source.element, section: source.section), to: IndexPath(row: target.element, section: target.section))
+                }
+            }
+        }
+    }
 	
 	private func _performBatchUpdates(_ updates: () -> Void) {
 		if #available(iOS 11.0, tvOS 11.0, *) {
